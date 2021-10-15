0cabeeb7f5e485fc46af66492840ac3d2f1ee309
skv-ctr <skv.ctr@tenable.com>
2021-10-16 01:57:22 +0530

    mongo db and model changes

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,11 +1,18 @@
 // IIFE -- Immediately Invoked Function Expression
-(function(){
+(function () {
 
-    function Start()
-    {
+    function Start() {
         console.log("App Started...");
     }
 
     window.addEventListener("load", Start);
 
 })();
+
+$(document).ready(function () {
+    if($('#dtBasicExample')){
+        $('#dtBasicExample').DataTable();
+        $('.dataTables_length').addClass('bs-select');
+    }
+    
+});
