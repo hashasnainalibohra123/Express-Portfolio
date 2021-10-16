80a6eb59c66a34ed53b89b17e304fbfa28529d30
skv-ctr <skv.ctr@tenable.com>
2021-10-16 12:57:12 +0530

    edit,add page css changes

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -11,7 +11,9 @@
 
 $(document).ready(function () {
     if($('#dtBasicExample')){
-        $('#dtBasicExample').DataTable();
+        $('#dtBasicExample').DataTable({
+            "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]]
+        } );
         $('.dataTables_length').addClass('bs-select');
     }
     
